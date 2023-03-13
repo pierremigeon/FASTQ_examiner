caa9fdc16244fac08151d1ec19d2369d111e706f
Pierre Migeon <pi3rre.migeon@gmail.com>
2023-03-13 03:18:00 -0700

    Create functions for generating common substrings for + separator and headerlines and functions to detect these lines in the input file

--- src/put_in_struct.py
+++ src/put_in_struct.py
@@ -51,47 +51,125 @@ def error_out(file_name, error_lines):
 			out.write(line + ("\n" * newline))
 	print("%d reads from file %s were determined to be erroneous, removed, and placed in %s" % (read_count, fname_base, out_file_name))
 
-#
 # this handles/detects reads that are:
-#	1) incomplete, 
-#	2) too many lines (5+)
+#	1) incomplete (too few lines), 
+#	2) too many lines (>4)
 #	3) lines out of order
-#	4) only legal characters used (nucleotides)
-#	4) Need to handle nucleotides replacing quality line in future
+#	4) only legal nucleotides
+#	5) 
 # And it unwraps lines for both quality and nucleotide, automatically.
 # Even if the lines are found out of order
 ########
 #TO DO LIST:
 # Next, I would like to run tests on these error detection steps to verify correct function. 
-# Then, I will also add some tests for the header region. 
-# Then, I will add details of output of incorrect files
 # Then, I would like to collect summary statistics and output these to tables.
 # It will probably be best to collect all the statistics as the reads are placed in the data structure, then send these summaries to the graphing functions at the end. 
-# Next, I would like to detect if headers are inconsistent within the file and between files,
+# I would like to detect if headers are inconsistent within the file and between files,
 # and I would like to remove singleton reads and place these in a seperate file.
 # Paired reads can be interleaved as well. Or they can all be output as is, minus the error reads.
 #
 
 
+from difflib import SequenceMatcher
+
+def longest_common_substring(s1: str, s2: str) -> str:
+    seq_matcher = SequenceMatcher(isjunk=None, a=s1, b=s2)
+    match = seq_matcher.find_longest_match(0, len(s1), 0, len(s2))
+    if match.size:
+        return s1[match.a : match.a + match.size]
+    else:
+        return ""
+
+def longest_common_substring_percentage(s1 : str, s2 : str) -> float:
+	if min(len(s1), len(s2)) == 0:
+		return 0
+	return len(longest_common_substring(s1, s2)) / min(len(s1), len(s2))
+
+def fresh_line(line, i):
+	if i >= 0:
+		return False
+	if not re.match('^@', line):
+		if not line.startswith("#"):
+			return True
+
+def: is_plus(line, info):
+	if not re.match('^\+', line):
+		return False
+	if len(line) < 0.5 * info[2] or len(line) > 0.5 * info[2]
+		return False
+	if len(line) > 10 and info[4] > 10:
+		if line in info[4] or info[4] in line:
+			return True
+	if longest_common_substring_percentage(line, info[3]) > 0.50:
+		return True
+	return False
+
+def is_header(line, info):
+	if not re.match('^@', line):
+		return False
+	if len(line) < 0.5 * info[1] or len(line) > 0.5 * info[1]
+		return False
+	if len(line) > 10 and info[1] > 10:
+		 if line in info[3] or info[3] in line:
+			return True
+	if longest_common_substring_percentage(line, info[3]) > 0.50:
+		return True
+	return False
+
+#output warning to apropriate locations, error log files or console
+def get_common_string(header, tag):
+	average_len = reduce(lambda x, y: x + y, map(len, header)) / len(header)
+	substr = ""
+	for i in average_len:
+		common_letter = max(set(header), key=header[i].count)[i]
+		filtered = [ h for h in head if h[i] == common_letter ]
+		if (len(filtered) / float(i)) * 100 < 75:
+			break
+		substr += common_letter
+	if len(substr) < 0.3 * average_len:
+		print("Warning: %s lines are extremely polymorphic." % \
+			("header" if tag == "@" else "separator"))
+	return substr
+
+def get_info(file):
+	head_len = plus_len = other_len = header = plus = []
+	i = 0
+	line = file.read()
+	while i < 4000 and line := file.read():
+		if re.match('^@', line):
+			head_len.append(len(line))
+			header.append(line)
+		elif re.match('^\+', line):
+			pluses.append(len(line))
+		else
+			other.append(len(line))
+		i += 1
+	head_len = max(set(head_len), key=head_len.count)
+	plus_len = max(set(pluses), key=pluses.count)
+	other_len = max(set(other), key=other.count)
+	header = get_commmon_string(header, "@")
+	plus = get_common_string(plus, "+")
+	return (other_len, head_len, plus_len, header, plus)
+
+#what happens if a quality line starts with @? Need a more concrete means of checking header
 #############################################
 # Place each file into array of dictionaries
 #############################################
 def put_in_struct(file_name):
-	head = re.compile('^@')
-	sep = re.compile('^\+')
 	lines = [{"filename" : file_name}]
 	error_lines = []
 	file = open(file_name, 'r')
 	i = -1
+	info = get_info(file)
+	file.seek(0)
 	for line in file:
-		if not head.match(line) and i == -1:
-			if not line.startswith("#"):
-				error_lines.append(line)			
-		if head.match(line):
+		if fresh_line(line, i):
+			error_lines.append(line)
+		if is_header(line, info)
 			i = 0
 			check_error(lines, error_lines)
 			lines.append(init_array(line))
-		elif sep.match(line):
+		elif is_plus(line, info):
 			i += 1
 			lines[-1][i] += line.rstrip()
 		else:
