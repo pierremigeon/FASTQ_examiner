f27affa7eed684155fe75aa958436b7b1837b8cc
Pierre Migeon <pi3rre.migeon@gmail.com>
2023-03-25 01:40:20 -0700

    remove singleton reads and output to corresponding file

--- src/manage_files.py
+++ src/manage_files.py
@@ -5,16 +5,28 @@
 #@
 #############################################@
 from itertools import chain
+import os
 
 def remove_singletons(seqs):
 	for i in range(0, len(seqs), 2):
 		if seqs[i][0]["paired"] == False:
 			break
-		diff = set(seqs[i][0]["headers"].keys()).symmetric_difference(seqs[i + 1][0]["headers"].keys())
-		out = [read for read in seqs[i][1:len(seqs[i])] if read[0] in diff]
-		with open('./singletons', 'w') as f:
-			f.write("\n".join(list(chain.from_iterable(out))))
+		file_1_name = os.path.splitext(os.path.basename(seqs[i][0]["filename"]))[0]
+		file_1_name = './out/' + file_1_name + '_singleton.fq'
+		file_2_name = os.path.splitext(os.path.basename(seqs[i + 1][0]["filename"]))[0]
+		file_2_name = './out/' + file_2_name + '_singleton.fq'
+		set1 = map(lambda head: head.strip("/[12]"), seqs[i][0]["headers"].keys())
+		set2 = map(lambda head: head.strip("/[12]"), seqs[i + 1][0]["headers"].keys())
+		diff = set(set1).symmetric_difference(set2)
+		out1 = [read for read in seqs[i][1:len(seqs[i])] if read[0].strip("/[12]") in diff]
+		out2 = [read for read in seqs[i + 1][1:len(seqs[i + 1])] if read[0].strip("/[12]") in diff]
+		with open(file_1_name, 'w') as f:
+			f.write("\n".join(list(chain.from_iterable(out1))))
 		f.close()
+		with open(file_2_name, 'w') as f:
+			f.write("\n".join(list(chain.from_iterable(out2))))
+		f.close()
+		
 
 def order_files(seqs):
 	seqs.sort(key=lambda x: x[0]['head'])
