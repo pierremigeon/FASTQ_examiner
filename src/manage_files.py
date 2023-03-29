774a6625ddb5a11d307da7a43109cf7c8ecec05c
Pierre Migeon <pi3rre.migeon@gmail.com>
2023-03-29 00:02:35 -0700

    refactor output singletons and abstract to seperate new function

--- src/manage_files.py
+++ src/manage_files.py
@@ -7,29 +7,28 @@
 from itertools import chain
 import os
 
+def get_file_out_name(original_file_name, extension):
+	out_name = os.path.splitext(os.path.basename(original_file_name))[0]
+	out_name = './out/' + out_name + extension
+	return out_name
+
 def output_processed_reads(seqs, leaf_flag):
-	#print(leaf_flag)
+	print(leaf_flag)
 
 def remove_singletons(seqs):
+	names, sets, out = [], [], []
 	for i in range(0, len(seqs), 2):
 		if seqs[i][0]["paired"] == False:
 			break
-		file_1_name = os.path.splitext(os.path.basename(seqs[i][0]["filename"]))[0]
-		file_1_name = './out/' + file_1_name + '_singleton.fq'
-		file_2_name = os.path.splitext(os.path.basename(seqs[i + 1][0]["filename"]))[0]
-		file_2_name = './out/' + file_2_name + '_singleton.fq'
-		set1 = map(lambda head: head.strip("/[12]"), seqs[i][0]["headers"].keys())
-		set2 = map(lambda head: head.strip("/[12]"), seqs[i + 1][0]["headers"].keys())
-		diff = set(set1).symmetric_difference(set2)
-		out1 = [read for read in seqs[i][1:len(seqs[i])] if read[0].strip("/[12]") in diff]
-		out2 = [read for read in seqs[i + 1][1:len(seqs[i + 1])] if read[0].strip("/[12]") in diff]
-		with open(file_1_name, 'w') as f:
-			f.write("\n".join(list(chain.from_iterable(out1))))
-		f.close()
-		with open(file_2_name, 'w') as f:
-			f.write("\n".join(list(chain.from_iterable(out2))))
-		f.close()
-		
+		for j in range(i, i + 2):
+			names.append(get_file_out_name(seqs[j][0]["filename"], '_singleton.fq'))
+			sets.append(map(lambda head: head.strip("/[12]"), seqs[j][0]["headers"].keys()))
+		diff = set(sets[i]).symmetric_difference(sets[i + 1])
+		for j in range(i, i + 2):
+			out.append([read for read in seqs[j][1:len(seqs[j])] if read[0].strip("/[12]") in diff])
+			with open(names[j], 'w') as f:
+				f.write("\n".join(list(chain.from_iterable(out[j]))))
+			f.close()
 
 def order_files(seqs):
 	seqs.sort(key=lambda x: x[0]['head'])
