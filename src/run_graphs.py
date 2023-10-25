c273e13834923fedee1c72fd9d0600cc1e167ff4
Pierre Migeon <pi3rre.migeon@gmail.com>
2023-10-24 20:07:13 -0700

    finish cumulative graph

--- src/run_graphs.py
+++ src/run_graphs.py
@@ -203,10 +203,7 @@ def quality_by_base(seqs, print_num):
 	for file in range(len(seqs)):
 		encoding = get_encoding(seqs[file])
 		sum = np.zeros((len(seqs[file]), 80), dtype=int)
-		if not len(t_sum):
-			t_sum = sum
-		else:
-			t_sum += sum
+		t_sum = sum if not len(t_sum) else np.vstack((t_sum, sum))
 		for entry in range(1, len(seqs[file]) - 1):
 			for i, base in enumerate(seqs[file][entry][3]):
 				sum[entry][i] = average_qual(base, encoding)
